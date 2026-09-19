4f1b471de115f0e041d4214dfa955735bfa218d2
Paulo Henrique Martins <paulonater2016@gmail.com>
2026-09-19 19:43:01 -0300

    refactory: as rotas layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,5 @@
-
-
 import Header from "@/app/_components/Header/header";
+import Footer from "../_components/footer";
 
 export default function MainLayout({
     children,
@@ -10,9 +9,10 @@ export default function MainLayout({
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow">
+            <main id="conteudo-principal" className="flex-grow">
                 {children}
             </main>
+            <Footer />
         </div>
     );
 }
