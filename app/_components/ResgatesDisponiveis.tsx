87fb0d2b496a3d5e1e969a1abee2a75fc0ca7a5d
Paulo Henrique Martins <paulonater2016@gmail.com>
2026-09-21 21:55:13 -0300

    refactory: import morto

--- app/_components/ResgatesDisponiveis.tsx
+++ app/_components/ResgatesDisponiveis.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { OfertaCard } from "../componentes/ui/OfertaCard";
-import { calcularTempoPostagem } from "../../lib/utils";
 import { prisma } from "@/lib/prisma"
 
 export default async function ResgatesDisponiveis() {
