4715fc04cc914f6cd9897142ab7253d4dea9126f
Paulo Henrique Martins <paulonater2016@gmail.com>
2026-09-21 22:35:37 -0300

    fix: Resolvendo o gargalo no Github Action

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI ? 'npm run start' : 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
